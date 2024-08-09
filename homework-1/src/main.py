c8f69ba1ba6b059a412a7511fa7ac7be14c8e9cb
Maria Fedorova <maria.i.fedorova@yandex.ru>
2024-08-09 14:06:56 +0300

    new files are added

---
+++ homework-1/src/main.py
@@ -0,0 +1,47 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from config import web_html_path
+from urllib.parse import urlparse, parse_qs
+
+# Для начала определим настройки запуска
+hostName = "localhost" # Адрес для доступа по сети
+serverPort = 8080 # Порт для доступа по сети
+
+class MyServer(BaseHTTPRequestHandler):
+    """
+        Специальный класс, который отвечает за
+        обработку входящих запросов от клиентов
+    """
+
+    def __get_web_html(self):
+        """Метод, считывающий web.html"""
+        with open(web_html_path, encoding='utf-8') as file:
+            web_html = file.read()
+        return web_html
+
+    def do_GET(self):
+        """ Метод для обработки входящих GET-запросов """
+        parser = parse_qs(urlparse(self.path).query)
+        print(parser)
+        web_html_content = self.__get_web_html()
+        self.send_response(200) # Отправка кода ответа
+        self.send_header("Content-type", "text/html") # Отправка типа данных, который будет передаваться
+        self.end_headers() # Завершение формирования заголовков ответа
+        self.wfile.write(bytes(web_html_content, "utf-8")) # Тело ответа
+
+
+if __name__ == "__main__":
+    # Инициализация веб-сервера, который будет по заданным параметрам в сети
+    # принимать запросы и отправлять их на обработку специальному классу, который был описан выше
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        # Cтарт веб-сервера в бесконечном цикле прослушивания входящих запросов
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        # Корректный способ остановить сервер в консоли через сочетание клавиш Ctrl + C
+        pass
+
+    # Корректная остановка веб-сервера, чтобы он освободил адрес и порт в сети, которые занимал
+    webServer.server_close()
+    print("Server stopped.")
